dcf21e0733da9fd61df186d7c08ed92733ea718d
bastisk <basti.sk@gmx.de>
2015-02-10 20:59:17 +0100

    Working at more Secure logon

--- js/app.js
+++ js/app.js
@@ -9,83 +9,111 @@ App.controller("TheCollectorCtrl", ["$scope", "Movielist", "$log", TheCollectorC
 App.controller("LoginCtrl", ["$scope", "$state", "$log", "$http", LoginCtrl]);
 
 // ========== CONTROLLERS ===========
-function TheCollectorCtrl($scope, Movielist, $log){
+function TheCollectorCtrl($scope, Movielist, $log) {
     $scope.movies = [];
     Movielist.getMovies($scope);
 }
 
 function LoginCtrl($scope, $state, UserList, $http) {
-    $scope.signIn = function(user) {
-    $http.get("http://basti-sk.com/php/users.php").success(function(result) {
+    $scope.signIn = function (user) {
         var UserName = user.username;
         var UserPassword = user.password;
         var UserPW_Hash = sha256_digest(UserPassword);
-        $scope.users = result;
-        for(i = 0; i < $scope.users.length; i++)
-        {
-            if($scope.users[i].PW_Hash == UserPW_Hash && $scope.users[i].Name == UserName) $state.go('tabs.movies');
-        }
-    });
-    };
+        var UserUN_Hash = sha256_digest(UserName);
+        $http.get("http://basti-sk.com/php/users.php?key1=" + UserUN_Hash + "&key2=" + UserPW_Hash).success(function (result) {
+                $scope.users = result;
+                if ($scope.users[0].Status == "true") $state.go('tabs.movies');
+                else alert("Error: Wrong Username or Password!");
+        });
+};
 }
 
 // ==========    ROUTES   ===========
-App.config(function($stateProvider, $urlRouterProvider) {
+App.config(function ($stateProvider, $urlRouterProvider) {
     $stateProvider
 
-            .state('home', {
-                url: '/',
-                template: '<p>Hello, world!</p>'
-            })
-            .state('login', {
-                url: "/login",
-                templateUrl: "templates/login.html",
-                controller: 'LoginCtrl'
-            })
-            .state('tabs', {
-                url: "/tab",
-                abstract: true,
-                templateUrl: "templates/tabs.html"
-            })
-            .state('tabs.movies', {
-              url: "/movies",
-              views: {
+        .state('home', {
+            url: '/',
+            template: '<p>Hello, world!</p>'
+        })
+        .state('login', {
+            url: "/login",
+            templateUrl: "templates/login.html",
+            controller: 'LoginCtrl'
+        })
+        .state('tabs', {
+            url: "/tab",
+            abstract: true,
+            templateUrl: "templates/tabs.html"
+        })
+        .state('tabs.movies', {
+            url: "/movies",
+            views: {
                 'movie-tab': {
-                  templateUrl: "templates/movies.html"
+                    templateUrl: "templates/movies.html"
                 }
-              }
-            })
-            .state('tabs.settings', {
-              url: "/settings",
-              views: {
+            }
+        })
+        .state('tabs.settings', {
+            url: "/settings",
+            views: {
                 'settings-tab': {
-                  templateUrl: "templates/settings.html"
+                    templateUrl: "templates/settings.html"
                 }
-              }
-            });
-        $urlRouterProvider.otherwise("/login");
-    });
+            }
+        });
+    $urlRouterProvider.otherwise("/login");
+});
 
 
 // ==========    SERVICES  ===========
-function Movielist($http, $log){
-    this.getMovies = function($scope) {
+function Movielist($http, $log) {
+    this.getMovies = function ($scope) {
         $scope.movies = [
-            {title: "Film A", genre: "Horror", watched: 0, voting: 1},
-            {title: "Film B", genre: "SciFi", watched: 4, voting: 3},
-            {title: "Film C", genre: "Documentation", watched: 0, voting: 6},
-            {title: "Film D", genre: "Action", watched: 2, voting: 2},
-            {title: "Film E", genre: "Action", watched: 2, voting: 5},
-            {title: "Film F", genre: "Horror", watched: 6, voting: 2}
+            {
+                title: "Film A",
+                genre: "Horror",
+                watched: 0,
+                voting: 1
+            },
+            {
+                title: "Film B",
+                genre: "SciFi",
+                watched: 4,
+                voting: 3
+            },
+            {
+                title: "Film C",
+                genre: "Documentation",
+                watched: 0,
+                voting: 6
+            },
+            {
+                title: "Film D",
+                genre: "Action",
+                watched: 2,
+                voting: 2
+            },
+            {
+                title: "Film E",
+                genre: "Action",
+                watched: 2,
+                voting: 5
+            },
+            {
+                title: "Film F",
+                genre: "Horror",
+                watched: 6,
+                voting: 2
+            }
         ];
     }
 }
 
-function UserList($http, $log, $q){
-    this.getUsers = function($scope) {
-        $http.jsonp("http://basti-sk.com/php/users.php").success(function(result) {
+function UserList($http, $log, $q) {
+    this.getUsers = function ($scope) {
+        $http.jsonp("http://basti-sk.com/php/users.php").success(function (result) {
             $scope.test = result;
         });
     };
 }
-
